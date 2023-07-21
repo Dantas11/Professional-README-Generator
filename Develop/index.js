63156874208d13122726a69808226a0c97e09ef1
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-07-21 19:59:15 +1000

    generateMarkdown function added

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const gm = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     "What is the project name?",
@@ -16,7 +17,11 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -70,7 +75,10 @@ function init() {
       }
   ])
 .then((answers) => {
-    console.log(answers)
+    const readmeContent = gm.generateMarkdown(answers);
+    writeToFile('README.md', readmeContent);
+
+    // console.log(answers)
     
   });
 }
